a4285959cfecdd8ce55f85d887548c3adf8be216
AD <silentflute.ad@gmail.com>
2025-10-14 22:12:31 +0100

    Rename file, creating empty test files

---
+++ SI_GameEngine/Tests/SI_GameEngineTests/GameModelTests.swift
@@ -0,0 +1,21 @@
+//
+//  GameModelTests.swift
+//  SI_GameEngine
+//
+//  Created by Amarjit on 14/10/2025.
+//
+
+import XCTest
+@testable import SI_GameEngine
+
+final class GameModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Setup
+    }
+
+    override func tearDownWithError() throws {
+        // Teardown
+    }
+
+}

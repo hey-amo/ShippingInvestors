a969d28ff2b9ccebe52e3956bbb520973aea1923
AD <silentflute.ad@gmail.com>
2025-10-16 15:49:52 +0100

    add blank turn order tests

---
+++ SI_GameEngine/Tests/SI_GameEngineTests/TurnOrderTests.swift
@@ -0,0 +1,21 @@
+//
+//  TurnOrderTests.swift
+//  SI_GameEngine
+//
+//  Created by Amarjit on 16/10/2025.
+//
+
+import XCTest
+
+@testable import SI_GameEngine
+
+final class TurnOrderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here.
+    }
+}

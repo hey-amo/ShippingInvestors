c89a03633b2a2b3fc2ef619f6dc62f0076d4e9c5
AD <silentflute.ad@gmail.com>
2025-10-22 08:44:33 +0100

    Add single dock view

---
+++ Shipping-Investors/Shipping-Investors/Views/SingleDockView.swift
@@ -0,0 +1,23 @@
+//
+//  SingleDockView.swift
+//  Shipping-Investors
+//
+//  Created by Amarjit on 22/10/2025.
+//
+
+import SwiftUI
+import SI_GameEngine
+
+struct DockViewModel {
+    let dock: Dock
+}
+
+struct SingleDockView: View {
+    var body: some View {
+        Text("Dock")
+    }
+}
+
+#Preview {
+    SingleDockView()
+}
